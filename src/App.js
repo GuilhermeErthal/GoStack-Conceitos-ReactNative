aeb1781823b34571a076282eb46f6187a46ceea5
GuilhermeErthal <guilhermeerthal@hotmail.com>
2020-06-21 13:18:10 -0300

    newFeatures

--- src/App.js
+++ src/App.js
@@ -35,6 +35,29 @@ export default function App() {
     setRepositories([...repositories])
 
   }
+  
+  async function handleAddRepository() {
+    
+    const response = await api.post('repositories', {      
+        title: "Desafio React2",
+        url: "https://github.com/GuilhermeErthal",
+        techs: ["Python", "Django"]      
+    });
+
+    const repository = response.data;
+
+    setRepositories([ ...repositories, repository])
+
+  }
+
+  async function handleRemoveRepository(id) {
+    await api.delete(`repositories/${id}`);
+
+    const response = repositories.filter(repository => repository.id !== id);
+
+    setRepositories(response);
+
+  }
 
   return (
     <>
@@ -67,17 +90,47 @@ export default function App() {
                 </Text>
               </View>
 
-              <TouchableOpacity
-                style={styles.button}
-                onPress={() => handleLikeRepository(repository.id)}                
-                testID={`like-button-${repository.id}`}
-              >
-                <Text style={styles.buttonText}>Curtir</Text>
-              </TouchableOpacity>
+              <View style={styles.containerButtons}>
+                <TouchableOpacity
+                  style={styles.buttonLike}
+                  onPress={() => handleLikeRepository(repository.id)}                
+                  testID={`like-button-${repository.id}`}
+                >
+                  <Text style={styles.buttonText}>Curtir</Text>
+                  
+                </TouchableOpacity>
+
+                <TouchableOpacity
+                  style={styles.buttonLike}
+                  onPress={() => handleLikeRepository(repository.id)}                
+                  testID={`like-button-${repository.id}`}
+                >                  
+                  <Text style={styles.buttonTextUnlike}>Descutir</Text>
+                </TouchableOpacity>
+
+                <TouchableOpacity
+                  style={styles.buttonLike}
+                  onPress={() => handleRemoveRepository(repository.id)}                
+                  testID={`like-button-${repository.id}`}
+                >                  
+                  <Text style={styles.buttonTextUnlike}>Remover Projeto</Text>
+                </TouchableOpacity>
+              </View>
+
             </View>
+
+            
           )}
         />
 
+        <TouchableOpacity 
+            activeOpacity={0.6} 
+            style={styles.buttonAdd} 
+            onPress={handleAddRepository}
+            >
+              <Text style={styles.buttonTextAdd}>Adicionar projeto</Text>
+        </TouchableOpacity>
+                  
       </SafeAreaView>
     </>
   );
@@ -121,8 +174,12 @@ const styles = StyleSheet.create({
     fontWeight: "bold",
     marginRight: 10,
   },
-  button: {
+  containerButtons: {
     marginTop: 10,
+    flexDirection: "row",
+  },
+  buttonLike: {
+    flexDirection: "row",
   },
   buttonText: {
     fontSize: 14,
@@ -131,5 +188,25 @@ const styles = StyleSheet.create({
     color: "#fff",
     backgroundColor: "#7159c1",
     padding: 15,
+  },
+  buttonTextUnlike: {
+    fontSize: 14,
+    fontWeight: "bold",
+    marginRight: 10,
+    color: "#fff",
+    backgroundColor: "#ff0000",
+    padding: 15,
+  },
+  buttonAdd: {
+    backgroundColor: '#44e500',
+    margin: 20,
+    height: 50,
+    borderRadius: 4,
+    justifyContent: 'center',
+    alignItems: 'center'
+  },
+  buttonTextAdd: {
+    fontWeight: 'bold',
+    fontSize: 16,
   },
 });
